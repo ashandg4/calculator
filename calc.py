945537d1ffc1cbbb7528fff065b702c73d449cb5
Ashu <ashunand8804@gmail.com>
2024-05-18 20:15:51 +0530

    add few features

--- calc.py
+++ calc.py
@@ -1,37 +1,45 @@
+#! /usr/bin/env python
+
 import sys
 
 from help import help
 from sum import sum
 from sub import sub
 
-arguments = sys.argv[1:]
 
-length = len(arguments)
+def main():
+    arguments = sys.argv[1:]
 
-if length == 0:
-    help()
-elif length >= 1:
-    if arguments[0] == "help":
+    length = len(arguments)
+
+    if length == 0:
         help()
-    elif arguments[0] == "sum":
-        if length == 3:
-            try:
-                num1 = int(arguments[1])
-                num2 = int(arguments[2])
-                print(f"{num1} + {num2} = {sum(num1, num2)}")
-            except Exception as error:
-                print(error)
+    elif length >= 1:
+        if arguments[0] == "help":
+            help()
+        elif arguments[0] == "sum":
+            if length == 3:
+                try:
+                    num1 = int(arguments[1])
+                    num2 = int(arguments[2])
+                    print(f"{num1} + {num2} = {sum(num1, num2)}")
+                except Exception as error:
+                    print(error)
+            else:
+                print("need 2 numbers")
+        elif arguments[0] == "sub":
+            if length == 3:
+                try:
+                    num1 = int(arguments[1])
+                    num2 = int(arguments[2])
+                    print(f"{num1} - {num2} = {sub(num1, num2)}")
+                except Exception as error:
+                    print(error)
+            else:
+                print("need 2 numbers")
         else:
-            print("need 2 numbers")
-    elif arguments[0] == "sub":
-        if length == 3:
-            try:
-                num1 = int(arguments[1])
-                num2 = int(arguments[2])
-                print(f"{num1} - {num2} = {sub(num1, num2)}")
-            except Exception as error:
-                print(error)
-        else:
-            print("need 2 numbers")
-    else:
-        print("command not found")
+            print("command not found")
+
+
+if __name__ == "__main__":
+    main()
